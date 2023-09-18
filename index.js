889265eb29c73e63b92e5ba6c53813e657082c50
Isa Vega <imvega100@gmail.com>
2023-09-17 21:33:12 -0400

    new save

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function mainMenu(){
         type:"list",
         message:"What would you like to do?",
         name:"selection",
-        choices:["view all departments", "view all roles","view all employees","add a department","add a role","add an employee","update an employee role"]
+        choices:["view all departments", "view all roles","view all employees","add a department","add a role","add an employee","update an employee role", "exit"]
     })
     .then(answer=>{
         
